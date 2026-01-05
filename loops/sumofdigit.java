6a542fdafed3345918925082f14343d3ab540187
mayankmalviya015 <mayankmalviya2620@gmail.com>
2026-01-05 11:10:55 +0530

    changes

--- loops/sumofdigit.java
+++ loops/sumofdigit.java
@@ -5,7 +5,6 @@ public class sumofdigit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        
         int sum = 0;
         while(n != 0){
             int ld = n% 10; // ld = lastdigit
