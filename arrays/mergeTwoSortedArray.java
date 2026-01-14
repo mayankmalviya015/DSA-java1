160376472ac7b4c2259db4b5cba812021f4c873e
mayankmalviya015 <mayankmalviya2620@gmail.com>
2026-01-14 13:00:19 +0530

    changes

--- arrays/mergeTwoSortedArray.java
+++ arrays/mergeTwoSortedArray.java
@@ -1,7 +1,7 @@
-public class mergeTwoShortedArray {
+public class mergeTwoSortedArray {
     public static void main(String[] args) {
         int[] arr ={12 , 34 , 45 ,58 ,68 ,79 };
-        int[] nums ={ 0 , 1 , 42 ,56 ,85 ,97 };
+        int[] nums ={ 0 , 11 , 42 ,56 ,85 ,97 };
         int a = arr.length;
         int b = nums.length;
         int[] br = new int[a +b];
@@ -18,20 +18,16 @@ public class mergeTwoShortedArray {
                j++;
         }
         }
-        if(i== a){
             while(j<b){
                 br[k]= nums[j];
                 k++;
                 j++;
             }
-        }
-          if(j==b){
             while(i<a){
                 br[k]= arr[i];
                 k++;
                 i++;
             }
-        }
         for(int ele : br){
             System.out.print(ele+ " ");
         }
