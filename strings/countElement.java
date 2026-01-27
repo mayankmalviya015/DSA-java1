f6a89dc03140124eaa7dab4be34350ab4175aaae
mayankmalviya015 <mayankmalviya2620@gmail.com>
2026-01-27 22:36:15 +0530

    add count file

---
+++ strings/countElement.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class countElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int n =sc.nextInt();
+        String s = Integer.toString(n);
+        System.out.println(s);
+        System.out.println(s.length());
+    }
+}
