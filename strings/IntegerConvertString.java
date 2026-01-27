d9754b9e6b0bebe8e281c33d7351a3f1966f5a10
mayankmalviya015 <mayankmalviya2620@gmail.com>
2026-01-27 19:01:38 +0530

    add IntegerConvertString file

---
+++ strings/IntegerConvertString.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class IntegerConvertString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int n = sc.nextInt();
+        String s ="";
+        s+= n;
+        System.out.println(s);
+    }
+}
--- strings/PrintAllSubstrings.java
+++ strings/PrintAllSubstrings.java
@@ -1,6 +1,6 @@
 public class PrintAllSubstrings {
     public static void main(String[] args) {
-        String s = "mayank";
+        String s = "gopal";
         for(int i = 0; i< s.length() ;i++){
             for(int j=i+1 ; j<= s.length() ; j++){
                 System.out.print(s.substring(i ,j) + " ");
