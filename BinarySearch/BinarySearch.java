98916b6cd05c2296421813ec3085e6d219e7f4f4
mayankmalviya015 <mayankmalviya2620@gmail.com>
2026-02-20 22:58:37 +0530

    add new files in binary search

--- BinarySearch/BinarySearch.java
+++ BinarySearch/BinarySearch.java
@@ -1,23 +1,29 @@
-public class BinarySearch{
+public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {1, 24 ,35 ,45 ,65 ,74 ,84 ,95};
-        int n =arr.length;
+        int n = arr.length;
         int target = 35;
         boolean flag = false;
-        int lo =0;
-        int hi =n-1;
-        for(int i =0; i<n;i++){
-            int mid = lo +hi/2;
-            if(arr[mid]>target)
-                hi =mid-1;
-              else if(arr[mid]<target)
-                lo =mid+1;
-            else if(arr[mid]==target){
-               flag = true;
-               break;}
+
+        int lo = 0;
+        int hi = n - 1;
+
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+
+            if (arr[mid] > target)
+                hi = mid - 1;
+            else if (arr[mid] < target)
+                lo = mid + 1;
+            else {
+                flag = true;
+                break;
+            }
         }
-        if(flag==true)
-            System.err.println("yes present this array");
-        else System.err.println("no target is not present");
+
+        if (flag)
+            System.out.println("Yes, present in this array");
+        else
+            System.out.println("Target is not present");
     }
 }
---
+++ BinarySearch/LowerBound.java
@@ -0,0 +1,20 @@
+public class LowerBound {
+    public static void main(String[] args) {
+        int[] arr = {1, 25 ,45 ,69 ,75 ,85 ,85 ,95 ,99};
+        int n =arr.length;
+        int lb = n;
+        int target =85;
+        int lo =0;
+        int hi =n-1;
+        while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
+            if(arr[mid]>=target){
+                lb = Math.min(mid, lb);
+                hi=mid-1;}
+                else
+                     {lo =mid+1;}
+            }
+            System.err.println(lb);
+        
+        }
+    }
